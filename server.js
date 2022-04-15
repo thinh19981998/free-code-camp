330951f4d17a6370debcec9aa684ae67dc4550a3
thinh19981998 <dpthinh1508@gmail.com>
2022-04-15 09:50:55 +0700

    init commit

--- server.js
+++ server.js
@@ -6,25 +6,41 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// your first API endpoint...
+// app.get('/api/hello', function (req, res) {
+//   res.json({ greeting: 'hello API' });
+// });
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get('/api', (req, res) => {
+  res.json({ unix: new Date().getTime(), utc: new Date().toGMTString() });
 });
 
-
+app.get('/api/:time', function (req, res) {
+  let { time } = req.params;
+  if (time.match(/\d{5,}/)) {
+    time = +time;
+  }
+  let date = new Date(time);
+  if (date.toUTCString() == 'Invalid Date') {
+    return res.json({ error: 'Invalid Date' });
+  }
+  res.json({
+    unix: date.valueOf(),
+    utc: date.toUTCString(),
+  });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
